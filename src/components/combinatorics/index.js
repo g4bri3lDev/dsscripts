329c7be040805633d6a221833ea1a6dc3673c326
ge65kob <M30s30b11k01>
2021-04-01 01:57:14 +0200

    Stirling 2nd kind calculator added

--- src/components/combinatorics/index.js
+++ src/components/combinatorics/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Topic from "../Topic";
 import PNK from "./PNK";
+import SNK from "./SNK";
 
 export default function Combinatorics() {
     return (
         <Topic title="Kombinatorik">
             <PNK title ="Pn,k Rechner:"/>
+            <SNK title ="Sn,k Rechner:"/>
         </Topic>
     )
 }
