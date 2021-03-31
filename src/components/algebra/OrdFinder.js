ff36a48637ed7e5ef70d8f8b57bd2c8b61ca0e12
tim <tim@finsol.de>
2021-03-31 19:54:33 +0200

    Small Changes for Ordnungs Finder

--- src/components/algebra/OrdFinder.js
+++ src/components/algebra/OrdFinder.js
@@ -7,18 +7,15 @@ export default function OrdFinder(props) {
     const [result, setResult] = useState(null)
     const [a, setA] = useState("")
     const [n, setN] = useState("")
-    const [p, setP] = useState("")
     return (
         <Card title={props.title}>
             <input value={a} onChange={event => setA(event.target.value)} type="number" placeholder="a"
                    style={inputStyle}/>
             <input value={n} onChange={event => setN(event.target.value)} type="number" placeholder="n"
                    style={inputStyle}/>
-            <input value={p} onChange={event => setP(event.target.value)} type="number" placeholder="p"
-                   style={inputStyle}/>
             <br/>
-            <button onClick={() => setResult(ord(n, a, p))}
-                    disabled={a === "0" || n === "0" || p === '0' || a === '' || a === "" || n === ""}>Calculate
+            <button onClick={() => setResult(ord(n, a))}
+                    disabled={a === "0" || n === "0" || a === "" || n === ""}>Calculate
             </button>
             <ul style={{textAlign: "left"}}>
                 {result != null ? result.dividers.map((value, index) =>
