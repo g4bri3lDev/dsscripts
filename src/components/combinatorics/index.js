e60c62068d3abd92dbb9e0a44c73158a2f0939a5
B14CK313 <jajoblack@gmx.de>
2021-04-02 00:49:13 +0200

    Fett Kombinatorik stuff 🎉🐧

--- src/components/combinatorics/index.js
+++ src/components/combinatorics/index.js
@@ -1,30 +1,70 @@
 import React from "react";
 import Topic from "../Topic";
 import GoldenListHead from "./GoldenListHead";
-import {ank, bnk, pnk, snk2} from "../../functions/combinatorics/allInOne";
+import {ank, bnk, cnk, dnk, enk, fnk, gnk, hnk, pnk, snk2, fact, pow, sumOfSnk} from "../../functions/combinatorics/allInOne";
 import AllInOne from "./AllInOne";
 
 // TODO: cnk, dnk, enk, fnk, gnk, hnk, sumOfSnk, fact
-export default function Combinatorics() {
+export default function Combinatorics(props) {
     return (
-        <Topic title="Kombinatorik">
+        <Topic title="Kombinatorik" containerStyle={{flexDirection: "column"}}>
             <AllInOne title ="All-in-one:"/>
-            <GoldenListHead title = "Sn,k Rechner:" function={snk2} text =
-                {["Bälle unterscheidbar",
-                    "Urnen gleich",
-                    "mind. 1 Ball pro Urne (surjektiv)"]}/>
-            <GoldenListHead title = "Pn,k Rechner:" function={pnk} text =
-                {["Bälle gleich",
-                "Urnen gleich",
-                "mind. 1 Ball pro Urne (surjektiv)"]}/>
-            <GoldenListHead title = "Fallende Faktorielle:" function={ank} text =
-                {["Bälle unterscheidbar",
-                    "Urnen unterscheidbar",
-                    "beliebig viele Bälle pro Urne"]}/>
-            <GoldenListHead title = "Binomialkoeffizient:" function={bnk} text =
-                {["Bälle gleich",
-                    "Urnen gleich",
-                    "höchstens ein Ball pro Urne (injektiv)"]}/>
+            <Topic title="Beliebig viele Bälle pro Urne (beliebig)">
+            	<GoldenListHead title="n^k Rechner:" function={pow} text={[
+            		"Bälle unterscheidbar",
+            		"Urnen unterscheidbar"
+            	]}/>
+            	<GoldenListHead title="Cn,k Rechner:" function={cnk} text={[
+            		"Bälle gleich",
+            		"Urnen unterscheidbar"
+            	]}/>
+				<GoldenListHead title="sum(Sn,k) Rechner:" function={sumOfSnk} text={[
+            		"Bälle unterscheidbar",
+            		"Urnen gleich"
+            	]}/>
+				<GoldenListHead title="Hn,k Rechner:" function={hnk} text={[
+            		"Bälle gleich",
+            		"Urnen gleich"
+            	]}/>
+            </Topic>
+            <Topic title="Höchstens ein Ball pro Urne (injektiv)">
+				<GoldenListHead title="An,k Rechner:" function={ank} text={[
+            		"Bälle unterscheidbar",
+            		"Urnen unterscheidbar"
+            	]}/>
+				<GoldenListHead title="Bn,k Rechner:" function={bnk} text={[
+            		"Bälle gleich",
+            		"Urnen unterscheidbar"
+            	]}/>
+			</Topic>
+			<Topic title="Mindestens ein Ball pro Urne (surjektiv)">
+				<GoldenListHead title="Fn,k Rechner:" function={fnk} text={[
+            		"Bälle unterscheidbar",
+            		"Urnen unterscheidbar"
+            	]}/>
+				<GoldenListHead title="Gn,k Rechner:" function={gnk} text={[
+            		"Bälle gleich",
+            		"Urnen unterscheidbar"
+            	]}/>
+				<GoldenListHead title="Sn,k Rechner:" function={snk2} text={[
+            		"Bälle unterscheidbar",
+            		"Urnen gleich"
+            	]}/>
+				<GoldenListHead title="Pn,k Rechner:" function={pnk} text={[
+            		"Bälle gleich",
+            		"Urnen gleich"
+            	]}/>
+			</Topic>
+			<Topic title="Genau ein Ball pro Urne (bijektiv)">
+				<GoldenListHead title="Factorial" function={fact} text={[
+            		"Bälle unterscheidbar",
+            		"Urnen unterscheidbar"
+            	]}/>
+			</Topic>
+			<Topic title="Sonstige">
+				<GoldenListHead title="Dn,k" function={dnk}/>
+				<GoldenListHead title="En,k" function={enk}/>
+			</Topic>
         </Topic>
     )
 }
