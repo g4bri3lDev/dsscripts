efc29586bd1dddd93bb99a6131f976cc7130c26f
Marvin Raiser <marvin_raiser@web.de>
2021-04-01 09:46:06 +0200

    get rid of px values -> they are evil!!

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,17 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import '../app.css'
 
 const style = {
     boxShadow: "0 4px 8px 0 rgba(0,0,0,1)",
-    margin: "5px",
-    padding: "16px",
+    margin: "0.5rem",
+    padding: "2rem",
     textAlign: "center",
     flexDirection: "column"
 }
 export default function Card(props) {
     const [collapsed, setCollapsed] = useState(!props.isCategory)
     return (
-        <div className="Card" style={{...props.style, ...style}}>
+        <div className="Card" style={{ ...props.style, ...style }}>
             <h1 onClick={() => props.isCategory ? setCollapsed(prev => !prev) : null}>{props.title}{props.isCategory ? collapsed ? " -" : " +" : null}</h1>
             {collapsed ?
                 <div style={props.style}>
