bbd60f8abf8a0f8eb390171d67ac6168fc0f1b49
B14CK313 <jajoblack@gmx.de>
2021-04-01 17:57:36 +0200

    Regex validation for Grapheneigenschaften

--- src/components/graphs/GraphProperties.js
+++ src/components/graphs/GraphProperties.js
@@ -3,12 +3,13 @@ import Card from "../Card";
 import {canBeRealized} from "../../functions/graphs/graphProperties";
 
 export default function GraphProperties(props) {
+	const validationRegex = /^\s*(\d+\s*,\s*)*\d+\s*$/;
     const [input, setInput] = useState("")
     const [result, setResult] = useState({canBeRealized: null, steps: []})
     return (
         <Card title={props.title}>
             ( <input value={input} onChange={(event) => setInput(event.target.value)}/> )
-            <button onClick={() => setResult(canBeRealized(input))}>Calculate</button>
+            <button className="statusButton" disabled={!validationRegex.test(input)} onClick={() => setResult(canBeRealized(input))}>Calculate</button>
             <ul>
                 <li>realisierbar: {result.canBeRealized != null ? result.canBeRealized ? " ✅" : " ❌" : null}</li>
                 <ul style={{textAlign: "left"}}>
