d15f2eda4eab6a03f2213ebe71007dca9d7249c8
B14CK313 <jajoblack@gmx.de>
2021-04-01 11:26:57 +0200

    Major improvement of the Gale-Shapley Card - better layout on mobile devices - better feedback for invalid input

--- src/components/graphs/GaleShapley.js
+++ src/components/graphs/GaleShapley.js
@@ -5,73 +5,82 @@ import {inputStyle} from "../../styles";
 
 // frag nicht... ich versteh es selbst nicht ganz
 export default function GaleShapley(props) {
-    const [table, setTable] = useState([[[1,3,4,2],[3,4,2,1]],[[4,1,3,2],[2,1,3,4]],[[4,3,1,2],[3,1,2,4]],[[3,4,1,2],[4,3,2,1]]]);
+    const [table, setTable] = useState([[[1,3,4,2],[4,1,3,2],[4,3,1,2],[3,4,1,2]],[[3,4,2,1],[2,1,3,4],[3,1,2,4],[4,3,2,1]]]);
     const [result, setResult] = useState(null);
     const [n, setN] = useState(4);
-    const [a, setA] = useState("Männer");
-    const [b, setB] = useState("Frauen");
+    const [types, setTypes] = useState(["Männer", "Frauen"]);
     const [prefix, setPrefix] = useState(false);
     const symbols = ["💍", "🧺", "💔"];
 
     // TODO: better defaults?
     const getDefault = (n) => {
         let newTable = [];
-        for(let i=0; i<n; i++) {
-            let row = [];
-            row[0] = [];
-            row[1] = [];
-            for(let j=0; j<n; j++){
-                row[0][j] = n-j;
-                row[1][j] = n-j;
-            }
-            newTable[i] = row;
+        for(let i=0; i<2; i++) {
+        	newTable[i] = [];
+        	for(let j=0; j<n; j++) {
+            	newTable[i][j] = [];
+            	for(let k=0; k<n; k++){
+                	newTable[i][j][k] = n-k;
+	            }
+    	    }
         }
         return newTable;
     }
 
     const swapSides = () => {
 		let newTable = [];
-		for(let i=0; i<n; i++) {
-            let row = [];
-            row[0] = table[i][1];
-            row[1] = table[i][0];
-            newTable[i] = row;
-        }
+        newTable[0] = [];
+        newTable[1] = [];
+        for(let j=0; j<n; j++) {
+        	newTable[0][j] = [];
+        	newTable[1][j] = [];
+           	for(let k=0; k<n; k++){
+             	newTable[0][j][k] = table[1][j][k];
+             	newTable[1][j][k] = table[0][j][k];
+	        }
+    	}
         setTable(newTable);
     }
 
-    const calculate = () => {
-    	let test = 0;
+    const validate = () => {
+		let test = 0;
     	for(let i=1; i<=n; i++) {
     		test += i;
     	}
-		for(let i=0; i<n; i++) {
-			if(table[i][0].reduce((a,b) => a+b, 0) !== test || table[i][1].reduce((a,b) => a+b, 0) !== test){
-				console.warn("Mistake in input detected!");
-				alert("Mistake in input detected!");
-				return;
-			}
-		}
+    	for(let i=0; i<2; i++) {
+        	for(let j=0; j<n; j++) {
+            	if(table[i][j].reduce((a,b) => a+b, 0) !== test){
+            		return false;
+            	}
+    	    }
+        }
+        return true;
+    }
+
+    const calculate = () => {
     	let r = gsRun(JSON.parse(JSON.stringify(table)), n);
     	if(r != null) setResult(r);
     }
 
-    const getAPrefix = () => {
-        return prefix ? a.charAt(0) : "";
+    const getPrefix = (type) => {
+        return prefix ? types[type].charAt(0) : "";
     }
-    const getBPrefix = () => {
-        return prefix ? b.charAt(0) : "";
-    }
-
+    
     const createTable = (n) => {
         setN(n);
         setTable(getDefault(n));
     }
 
-    const setCell = (row,gen,col,val) => {
+	const setType = (type, value) => {
+		let newTypes = types.slice();
+		newTypes[type] = value;
+		setTypes(newTypes);
+	}
+
+    const setCell = (type,row,col,val) => {
         // dirty way to create deep copy
         let newTable = JSON.parse(JSON.stringify(table));
-        newTable[row][gen][col] = parseInt(val);
+        newTable[type][row][col] = parseInt(val);
         setTable(newTable);
     }
 
@@ -80,60 +89,60 @@ export default function GaleShapley(props) {
             <p>
                 Matche
                 <input size="2" value={n} min="1" onChange={event => createTable(event.target.value)} type="number" placeholder="n" style={inputStyle}/>
-                <input size="10" value={a} onChange={event => setA(event.target.value)} type="text" placeholder="a" style={inputStyle}/>
+                <input size="10" value={types[0]} onChange={event => setType(0, event.target.value)} type="text" placeholder="a" style={inputStyle}/>
                 mit
-                <input size="10" value={b} onChange={event => setB(event.target.value)} type="text" placeholder="b" style={inputStyle}/>
-                ({a} machen {b} Antrag)
+                <input size="10" value={types[1]} onChange={event => setType(1, event.target.value)} type="text" placeholder="b" style={inputStyle}/>
+                <br/>
+                ({types[0]} machen {types[1]} Antrag)
             </p>
+            <div style={{display: "flex", flexFlow: "wrap"}}>
+           	{table.map((type, typeId) =>
             <table>
                 <thead>
                 <tr>
-                    <th></th>
-                    <th colSpan={n}>{a}</th>
-                    <th colSpan={n}>{b}</th>
+                    <th colSpan={n}>{types[typeId]}</th>
                 </tr>
                 </thead>
                 <tbody>
-                {table != null ? table.map((row, rowid) =>
-                    <tr>
-                        <td>{rowid+1}</td>
-                        {row.map((gender, genderid) => gender.map((col,colid) =>
-                            <td><input size="2" value={col} min="1" max={n} onChange={event => setCell(rowid,genderid,colid,event.target.value)} type="number" style={inputStyle}/></td>
-                        ))}
-                    </tr>
-                ) : null}
+					{type.map((row, rowId) =>
+                	<tr>
+               			{row.map((col, colId) =>
+                   		<td><input size="2" value={col} min="1" max={n} onChange={event => setCell(typeId,rowId,colId,event.target.value)} type="number" style={inputStyle}/></td>
+                		)}
+                	</tr>
+                	)}
                 </tbody>
             </table>
+            )}
+            </div>
             <p>
                 <button onClick={() => setPrefix(!prefix)}>Toggle Prefix</button>&nbsp;
 				<button onClick={() => swapSides()}>Swap Sides</button>&nbsp;
-                <button onClick={() => calculate()}>Calculate</button>
+                <button onClick={() => calculate()} disabled={!validate()} className="statusButton">Calculate</button>
             </p>
-            <table>
+            <table className="narrowTable">
                 <thead>
                 <tr>
-                    <th>Schritt</th>
+                    <th></th>
                     <th>Verlobung</th>
-                    <th colSpan={n}>{a} verlobt mit</th>
-                    <th colSpan={n}>{b} verlobt mit</th>
+                    {table.map((_,typeId) => <th colSpan={n}>{types[typeId]} verlobt mit</th>)}
                 </tr>
                 <tr>
-                    <td></td>
-                    <td></td>
-                    {table.map((_,i) => <td>{getAPrefix()}{i+1}</td>)}
-                    {table.map((_,i) => <td>{getBPrefix()}{i+1}</td>)}
+                    <th></th>
+                    <th></th>
+                    {table.map((type,typeId) => type.map((_, j) => <th>{getPrefix(typeId)}{j+1}</th>))}
                 </tr>
                 </thead>
                 <tbody>
                 {result != null ? result.map((row, rowid) =>
                     <tr>
                         <td>{rowid+1}</td>
-                        <td>{getAPrefix()}{row[0][0]+1} ➡ {getBPrefix()}{row[0][1]+1} <span style={{"fontSize":"7pt"}}>{symbols[row[0][2]]}</span></td>
+                        <td>{getPrefix(0)}{row[0][0]+1} ➡ {getPrefix(1)}{row[0][1]+1} <span style={{fontSize:".5em"}}>{symbols[row[0][2]]}</span></td>
                         {row[1].map((val) =>
-                            <td>{val >= 0 ? getBPrefix()+(val+1) : ""}</td>
+                            <td>{val >= 0 ? getPrefix(1)+(val+1) : ""}</td>
                         )}
                         {row[2].map((val) =>
-                            <td>{val >= 0 ? getAPrefix()+(val+1) : ""}</td>
+                            <td>{val >= 0 ? getPrefix(0)+(val+1) : ""}</td>
                         )}
                     </tr>
                 ) : null}
@@ -144,15 +153,16 @@ export default function GaleShapley(props) {
             </p>
             <ul>
                 {result != null ? result[result.length-1][1].map((b, a) =>
-                    <li>{getAPrefix()+(a+1)} → {getBPrefix()+(b+1)}</li>
+                    <li>{getPrefix(0)+(a+1)} → {getPrefix(1)+(b+1)}</li>
                 ) : null}
             </ul>
-            <p style={{"fontSize":"7pt"}}>
+            <p style={{fontSize:".5em"}}>
                 Symbole:<br/>
                 {symbols[0]} Verlobung<br/>
                 {symbols[1]} Korb<br/>
                 {symbols[2]} Trennung+Verlobung<br/>
-                Keine Garantie auf die Korrektheit der Ergebnisse!</p>
+                Keine Garantie auf die Korrektheit der Ergebnisse!
+			</p>
         </Card>
     )
 }
