acc717215a693e6be58d6846ce83326efabda766
gabriel <g.lackermeier@gmx.de>
2021-04-01 17:40:25 +0200

    new card design

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,8 @@ import React, {useState} from "react";
 import '../app.css'
 
 const style = {
-    boxShadow: "0 4px 8px 0 rgba(0,0,0,1)",
+    // boxShadow: "0 4px 8px 0 rgba(0,0,0,1)",
+    border: "1px solid var(--primaryTextColor)",
     margin: "0.5rem",
     padding: "2rem",
     textAlign: "center",
