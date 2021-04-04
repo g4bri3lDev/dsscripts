5b6e9d083c09cf9ef56c1c9d73495611fe1447f0
Marvin Raiser <marvin_raiser@web.de>
2021-04-04 20:24:10 +0200

    reduce padding of cards to optimize for mobile

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,10 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import '../app.css'
 
 const style = {
     boxShadow: "0 4px 8px 0 rgba(0,0,0,1)",
     margin: "0.5rem",
-    padding: "2rem",
+    padding: "1rem",
     textAlign: "center",
     flexDirection: "column"
 }
@@ -12,7 +12,7 @@ export default function Card(props) {
     const [collapsed, setCollapsed] = useState(!props.isCategory)
     return (
         <div className="Card" style={{ ...props.style, ...style }}>
-            <h1 style={{userSelect: "none"}}
+            <h1 style={{ userSelect: "none" }}
                 onClick={() => props.isCategory ? setCollapsed(prev => !prev) : null}>{props.title}{props.isCategory ? collapsed ? " -" : " +" : null}</h1>
             {collapsed ?
                 <div style={props.style}>
