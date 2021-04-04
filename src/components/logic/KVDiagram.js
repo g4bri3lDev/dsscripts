0be445a2b35b4f81e1fc76c41a5f107493bf679a
gabriel <g.lackermeier@gmx.de>
2021-04-05 00:35:20 +0200

    started logic

---
+++ src/components/logic/KVDiagram.js
@@ -0,0 +1,9 @@
+import Card from "../Card";
+
+export default function KVDiagram({title}) {
+    return (
+        <Card title={title}>
+
+        </Card>
+    )
+}
---
+++ src/components/logic/index.js
@@ -0,0 +1,11 @@
+import Topic from "../Topic";
+import KVDiagram from "./KVDiagram";
+
+export default function Logic() {
+    return (
+        <Topic title="Logik">
+            <KVDiagram title="KV Diagramm:"/>
+        </Topic>
+    )
+
+}
