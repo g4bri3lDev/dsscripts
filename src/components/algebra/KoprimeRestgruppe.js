c020eb83ed251188e90c6266634798ed1c6a3b07
B14CK313 <jajoblack@gmx.de>
2021-04-09 18:21:21 +0200

    Removed unused imports

--- src/components/algebra/KoprimeRestgruppe.js
+++ src/components/algebra/KoprimeRestgruppe.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import {printPhiFormula} from "../../functions/algebra/phi";
-import {lcmm, primeFactors, calcGroup, calcElemGroups, isCyclic} from "../../functions/algebra/koprimeRestgruppe";
+import {primeFactors, calcGroup, calcElemGroups, isCyclic} from "../../functions/algebra/koprimeRestgruppe";
 import {printPrimeFactors, printArrayWithSep} from "../../functions/utils";
 import Card from "../Card";
 import {inputStyle} from "../../styles";
@@ -9,7 +9,7 @@ export default function KoprimeRestgruppe(props) {
     const [factors, setFactors] = useState(null)
     const [group, setGroup] = useState(null)
     const [elemGroups, setElemGroups] = useState([]);
-    const [elemGroupsOrd, setElemGroupsOrd] = useState([]);
+    //const [elemGroupsOrd, setElemGroupsOrd] = useState([]);
     const [n, setN] = useState("")
 
     useEffect(() => {
@@ -18,7 +18,7 @@ export default function KoprimeRestgruppe(props) {
         setGroup(g);
         let eg = calcElemGroups(g, n);
         setElemGroups(eg)
-        setElemGroupsOrd(eg.map((elemGroup) => elemGroup.length));
+        //setElemGroupsOrd(eg.map((elemGroup) => elemGroup.length));
     }, [n]);
 
     return (
